79912f3cd8b392bfdd2f1a81c7698011f4f4730c
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-02-07 16:08:17 -0500

    [Update] - Validate text field and show message if the field is empty.

--- js/app.js
+++ js/app.js
@@ -23,5 +23,31 @@ function eventListeners() {
 
 function agregarTweet(e) {
   e.preventDefault();
-  console.log('Agregando Tweet');
+
+  //Textarea donde el usuario escribe.
+  const tweet = document.querySelector('#tweet').value;
+
+  //Validación.
+  if ( tweet.trim() === '' ) {
+    mostrarError('Tu tweet no puede ir vacio. Escribe algo.');
+    return; //Evita que se ejecuten mas lineas de codigo.
+  }
+
+  console.log(tweet);
+
+}
+
+//Crear y mostrar el mensaje de error.
+function mostrarError(mensaje){
+  const mensajeError = document.createElement('P');
+  mensajeError.textContent = mensaje;
+  mensajeError.classList.add('error');
+
+  const contenido = document.querySelector('#contenido')
+  contenido.appendChild(mensajeError)
+  
+  //Elimina la alerta dspues de 3 segundos.
+  setTimeout(() => {
+    mensajeError.remove();
+  }, 2000);
 }
